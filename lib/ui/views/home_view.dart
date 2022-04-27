0e2ca05fd65c136dffed1d25075bea0dcee5d819
Chammoun Chakhap <43007452+ChammounC@users.noreply.github.com>
2022-04-27 21:08:44 +0530

    Add files via upload

---
+++ lib/ui/views/home_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_menu/core/viewmodels/home_model.dart';
+import 'package:provider/provider.dart';
+import '../../widgets/liquid.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<HomeModel>(context);
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              height: model.isOpening?20:0,
+              child: const Text('Gandu'),
+            ),
+            Liquid(
+              isFlipped: true,
+              controller: _animationController,
+            ),
+            Liquid(
+              isFlipped: false,
+              controller: _animationController,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/liquid.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_menu/core/viewmodels/home_model.dart';
+import 'package:lottie/lottie.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:provider/provider.dart';
+
+import '../ui/shared/globals.dart';
+
+class Liquid extends StatelessWidget {
+  final AnimationController controller;
+  final bool isFlipped;
+  const Liquid({Key? key, required this.controller, required this.isFlipped})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final model = Provider.of<HomeModel>(context);
+    return Center(
+        child: AnimatedContainer(
+      duration: Duration(milliseconds: 2000),
+      curve: Curves.elasticOut,
+      transform: Matrix4.identity()
+        ..translate(
+            0.0, isFlipped ? -model.openValue - 100 : model.openValue + 100),
+      decoration:const BoxDecoration(
+        // color: Global.bgColor,
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Transform(
+            transform: Matrix4.identity()
+              ..scale(1.0, isFlipped ? -1.0 : 1.0)
+              ..translate(0.0, isFlipped ? -200 * 2 + 50 : -200 + 50),
+            child: Lottie.asset('data/liquid.json',
+                animate: false,
+                controller: controller,
+                height: 200,
+                delegates: LottieDelegates(values: [
+                  ValueDelegate.color(const ['**', 'Rectangle 1', 'Fill 1'],
+                      value: Colors.pinkAccent),
+                  ValueDelegate.color(const ['**', 'Shape 1', 'Fill 1'],
+                      value: Colors.pinkAccent),
+                ])),
+          ),
+          isFlipped
+              ? SizedBox()
+              : GestureDetector(
+                  onTap: () {
+                    model.openLiquidMenu(controller);
+                  },
+                  child: SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: Neumorphic(
+                      curve: Curves.bounceInOut,
+                      style:  NeumorphicStyle(
+                        color: Colors.pinkAccent,
+                        intensity: 0.6,
+                        shape: NeumorphicShape.concave,
+                        // oppositeShadowLightSource: true,
+                        depth:  model.isOpening?0:5,
+                      ),
+                      child: Icon(
+                        model.isOpening?Icons.lock_open:Icons.lock,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    ));
+  }
+}
